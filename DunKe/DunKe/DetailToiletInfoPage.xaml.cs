106eeba28eee98bc3e79f4869587ef47cf02d6af
2381553155 <2381553155@qq.com>
2015-03-13 14:43:00 +0800

    WP 2015-03-13 14:42
    初次版本

---
+++ DunKe/DunKe/DetailToiletInfoPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace DunKe
+{
+    public partial class DetailToiletInfoPage : PhoneApplicationPage
+    {
+        public DetailToiletInfoPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            try
+            {
+                string toilet = this.NavigationContext.QueryString["id"];
+                MessageBox.Show("该厕所的id号为："+toilet);
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+           
+        }
+    }
+}
